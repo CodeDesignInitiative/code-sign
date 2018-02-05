dc9cc1a930974164786c9e7b6ff9554493fb4c60
Fronbasal <danielmalik@outlook.de>
2018-02-05 22:57:58 +0100

    Attempted to change ModelAdmin

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from sign.models import Signature
+
+
+class SignatureAdmin(admin.ModelAdmin):
+    model = Signature
+
+    def admin_image(self, obj):
+        return format_html("<img src='{}'/>", obj.signature)
+
+    admin_image.short_description = "Signature"
+    admin_image.admin_order_field = 'last_name'
+
+    list_display = [admin_image, ]
+
+
+admin.site.register(Signature, SignatureAdmin)
