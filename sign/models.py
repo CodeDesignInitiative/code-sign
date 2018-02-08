2e5b3e27338355fedb592d983d8ec4e088d84a1e
Fronbasal <danielmalik@outlook.de>
2018-02-08 23:46:30 +0100

    feat: added timestamp to model

--- sign/models.py
+++ sign/models.py
@@ -5,6 +5,7 @@ class Signature(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     signature = models.CharField(max_length=65536)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return "{} {}".format(self.first_name, self.last_name)
+        return "{} {}  at {}".format(self.first_name, self.last_name, self.date)
